dfaeb99db91b25f95c58861739f5c0750602f088
Abdalrahman Tarek <abdalrhmntarqthmant132004@gmail.com>
2026-04-30 21:37:33 +0300

    sent to Whats app  group

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ if gpus:
 # --- GLOBAL VARIABLES ---
 last_results = []
 is_processing = False  # Flag to prevent multiple threads from running at once
-db_path = r"C:\Users\la_user\Desktop\stat_ai_DF\DB"
+db_path = r"DB"
 file = open("emotions.csv", "a", newline="", encoding="utf-8")
+frame_count = 0
 
 def save_to_csv(writer, student_id, emotion, confidence, lecture_id):
     time_now = datetime.now().strftime("%H:%M:%S")
@@ -71,24 +72,23 @@ while True:
     if not ret: break
 
     h_orig, w_orig = frame.shape[:2]
-    x_ratio, y_ratio = w_orig / target_width, h_orig / target_height
+    x_ratio, y_ratio = w_orig / target_width , h_orig / target_height
 
     # Kick off a background thread if we aren't already processing
-    if not is_processing:
+    if not is_processing and frame_count % 5 == 0:
         is_processing = True
         small_frame = cv2.resize(frame, (target_width, target_height))
         thread = threading.Thread(target=background_analysis, args=(small_frame,), daemon=True)
         thread.start()
-
     # Always draw the last known results (results will "jump" to new faces when thread finishes)
     for res in last_results:
         region = res['region']
         x, y, w, h = int(region['x'] * x_ratio), int(region['y'] * y_ratio), int(region['w'] * x_ratio), int(region['h'] * y_ratio)
-        save_to_csv(writer, res['identity_name'], res['emotion'], res['confidence'], "L1")
+        save_to_csv(writer, res['identity_name'],res['dominant_emotion'],res['emotion'][res['dominant_emotion']], "L1")
         label = f"{res['identity_name']} | {res['dominant_emotion']}"
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-
+    frame_count += 1
     cv2.imshow('Real-Time (Threading Enabled)', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'): break
 
